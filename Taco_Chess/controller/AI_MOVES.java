6032a6dd5006ce5283d28e0205301a0693b1c723
Jonathan Maus <jm-191091@hs-weingarten.de>
2020-08-25 21:39:14 +0200

    random

---
+++ Taco_Chess/controller/AI_MOVES.java
@@ -0,0 +1,16 @@
+package Taco_Chess.controller;
+
+import Taco_Chess.Figures.Abstract_Figure;
+import javafx.scene.control.Button;
+
+public class AI_MOVES
+{
+    public AI_MOVES()
+    {
+        System.out.println("constructor ai");
+    }
+    public void AI_MOVE()
+    {
+        System.out.println("ai moves");
+    }
+}
